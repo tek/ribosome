d29c8b3772b8ce6a7f7a0425a1b1b91a33f94d7b
Torsten Schmits <torstenschmits@gmail.com>
2018-12-28 17:47:19 +0100

    prefix echoes with plugin name

--- lib/Ribosome/Api/Echo.hs
+++ lib/Ribosome/Api/Echo.hs
@@ -5,14 +5,16 @@ module Ribosome.Api.Echo(
 
 import Neovim (vim_command')
 import Ribosome.Control.Ribo (Ribo)
+import qualified Ribosome.Control.Ribo as Ribo (name)
 
 escapeQuotes :: Char -> String
 escapeQuotes '\'' = "''"
 escapeQuotes a = [a]
 
 echom :: String -> Ribo e ()
-echom msg =
-  vim_command' $ "echom '" ++ concatMap escapeQuotes msg ++ "'"
+echom msg = do
+  name <- Ribo.name
+  vim_command' $ "echom '" ++ name ++ ": " ++ concatMap escapeQuotes msg ++ "'"
 
 echomS :: Show a => a -> Ribo e ()
 echomS = echom . show
