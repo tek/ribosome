6386c2ae83b96999f39e25cc276f57e92e32d50e
Torsten Schmits <torstenschmits@gmail.com>
2019-01-29 19:58:15 +0100

    `Log.err`

--- lib/Ribosome/Log.hs
+++ lib/Ribosome/Log.hs
@@ -1,18 +1,22 @@
 module Ribosome.Log(
   debug,
   info,
+  err,
   p,
   prefixed,
 ) where
 
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Neovim.Log (debugM, infoM)
+import Neovim.Log (debugM, infoM, errorM)
 
 debug :: (MonadIO m, Show a) => String -> a -> m ()
 debug name message = liftIO $ debugM name $ show message
 
 info :: (MonadIO m, Show a) => String -> a -> m ()
 info name message = liftIO $ infoM name $ show message
+
+err :: (MonadIO m, Show a) => String -> a -> m ()
+err name message = liftIO $ errorM name $ show message
 
 p :: (MonadIO m, Show a) => a -> m ()
 p = liftIO . print
