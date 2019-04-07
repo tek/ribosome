1f4351c38de59c7c6e2ae7d5727875e015b062b5
Torsten Schmits <torstenschmits@gmail.com>
2019-04-07 16:51:37 +0200

    test helpers for retrying an assertion

--- modules/core/lib/Ribosome/Control/Concurrent/Wait.hs
+++ modules/core/lib/Ribosome/Control/Concurrent/Wait.hs
@@ -1,12 +1,14 @@
 module Ribosome.Control.Concurrent.Wait where
 
-import Control.Exception.Lifted (SomeException(..), try)
+import Control.Exception.Lifted (Exception, SomeException(..), try)
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Trans.Control (MonadBaseControl)
 import Data.Default (Default(def))
+import Data.Functor ((<&>))
 
 import Ribosome.System.Time (sleep)
 
+-- |Specifies the maximum number of retries and the interval in seconds for 'waitIO'.
 data Retry =
   Retry Int Double
   deriving Show
@@ -14,25 +16,87 @@ data Retry =
 instance Default Retry where
   def = Retry 30 0.1
 
-waitIO :: (MonadIO m, MonadBaseControl IO m) => Retry -> m a -> (a -> m Bool) -> m (Either String a)
+-- |Error description for 'waitIO'
+data WaitError =
+  NotStarted
+  |
+  ConditionUnmet String
+  |
+  ∀ e. Exception e => Thrown e
+
+instance Show WaitError where
+  show NotStarted =
+    "NotStarted"
+  show (ConditionUnmet reason) =
+    "ConditionUnmet(" ++ reason ++ ")"
+  show (Thrown _) =
+    "Thrown"
+
+-- |Execute an IO thunk repeatedly until either the supplied condition produces a 'Right' or the maximum number of
+-- retries specified in the `Retry` parameter has been reached.
+-- Returns the value produced by the condition.
+waitIO ::
+  MonadIO m =>
+  MonadBaseControl IO m =>
+  Retry ->
+  m a ->
+  (a -> m (Either String b)) ->
+  m (Either WaitError b)
 waitIO (Retry maxRetry interval) thunk cond =
-  wait maxRetry (Left "initial")
+  wait maxRetry (Left NotStarted)
   where
     wait 0 reason = return reason
     wait count _ = do
       ea <- try thunk
-      result <- check ea
+      result <- try $ check ea
       case result of
-        Right a -> return $ Right a
-        Left reason -> do
-          sleep interval
-          wait (count - 1) (Left reason)
-    check (Right a) = do
-      ok <- cond a
-      return $ if ok then Right a else Left "condition unmet"
-    check (Left (SomeException _)) =
-      return $ Left "exception"
+        Right (Right a) ->
+          return $ Right a
+        Right (Left reason) ->
+          recurse reason count
+        Left (SomeException e) ->
+          recurse (Thrown e) count
+    recurse reason count = do
+      sleep interval
+      wait (count - 1) (Left reason)
+    check (Right a) =
+      cond a <&> \case
+        Right b -> Right b
+        Left reason -> Left (ConditionUnmet reason)
+    check (Left (SomeException e)) =
+      return $ Left (Thrown e)
 
-waitIODef :: (MonadIO m, MonadBaseControl IO m) => m a -> (a -> m Bool) -> m (Either String a)
+-- |Calls 'waitIO' with the default configuration of 30 retries every 100ms.
+waitIODef ::
+  MonadIO m =>
+  MonadBaseControl IO m =>
+  m a ->
+  (a -> m (Either String b)) ->
+  m (Either WaitError b)
 waitIODef =
   waitIO def
+
+-- |Same as 'waitIO', but the condition returns 'Bool' and the result is the result of the thunk.
+waitIOPred ::
+  MonadIO m =>
+  MonadBaseControl IO m =>
+  Retry ->
+  m a ->
+  (a -> m Bool) ->
+  m (Either WaitError a)
+waitIOPred retry thunk pred' =
+  waitIO retry thunk cond
+  where
+    cond a = pred' a <&> \case
+      True -> Right a
+      False -> Left "predicate returned False"
+
+-- |Calls 'waitIOPred' with the default configuration of 30 retries every 100ms.
+waitIOPredDef ::
+  MonadIO m =>
+  MonadBaseControl IO m =>
+  m a ->
+  (a -> m Bool) ->
+  m (Either WaitError a)
+waitIOPredDef =
+  waitIOPred def
---
+++ modules/test/lib/Ribosome/Test/Await.hs
@@ -0,0 +1,45 @@
+module Ribosome.Test.Await where
+
+import Control.Exception (throw)
+import Control.Monad.Error.Class (MonadError, catchError)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Control (MonadBaseControl)
+import Control.Monad.Trans.Except (ExceptT, runExceptT)
+import Test.Framework.AssertM (AssertM)
+
+import Ribosome.Control.Concurrent.Wait (WaitError(Thrown), waitIODef)
+
+await ::
+  Show e =>
+  MonadError e m =>
+  MonadIO m =>
+  MonadBaseControl IO m =>
+  AssertM m =>
+  (a -> m b) ->
+  m a ->
+  m b
+await assertion acquire = do
+  r <- waitIODef acquire' check'
+  either failure return r
+  where
+    acquire' = catchError (Right <$> acquire) (return . Left . show)
+    check' (Right a) = Right <$> assertion a
+    check' (Left e) = return (Left e)
+    failure (Thrown e) = throw e
+    failure e = fail $ "await failed with " ++ show e
+
+await' ::
+  ∀ a b m.
+  MonadIO m =>
+  MonadBaseControl IO m =>
+  AssertM (ExceptT () m) =>
+  (a -> m b) ->
+  m a ->
+  m b
+await' assertion acquire = do
+  r <- runExceptT $ await assertion' (lift acquire)
+  either (const $ fail "internal error") return r
+  where
+    assertion' :: a -> ExceptT () m b
+    assertion' = lift . assertion
