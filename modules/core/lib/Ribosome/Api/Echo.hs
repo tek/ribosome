ef6166ec30989dc87206abf41d15b19e7987ba9b
Torsten Schmits <torstenschmits@gmail.com>
2019-04-08 19:24:04 +0200

    provide version of `echo` without `MonadRibo`

--- modules/core/lib/Ribosome/Api/Echo.hs
+++ modules/core/lib/Ribosome/Api/Echo.hs
@@ -1,20 +1,22 @@
-module Ribosome.Api.Echo(
-  echom,
-  echomS,
-  escapeQuotes,
-) where
+module Ribosome.Api.Echo where
 
-import Control.Monad.DeepError (MonadDeepError)
-
-import Ribosome.Control.Monad.Ribo (MonadRibo, Nvim, pluginName)
+import Ribosome.Control.Monad.Ribo (MonadRibo, NvimE, pluginName)
 import Ribosome.Data.String (escapeQuotes)
 import Ribosome.Nvim.Api.IO (vimCommand)
-import Ribosome.Nvim.Api.RpcCall (RpcError)
 
-echom :: (MonadDeepError e RpcError m, MonadRibo m, Nvim m) => String -> m ()
+echom' :: NvimE e m => String -> m ()
+echom' msg =
+  vimCommand $ "echom '" ++ concatMap escapeQuotes msg ++ "'"
+
+echom :: MonadRibo m => NvimE e m => String -> m ()
 echom msg = do
   name <- pluginName
-  vimCommand $ "echom '" ++ name ++ ": " ++ concatMap escapeQuotes msg ++ "'"
+  echom' $ name ++ ": " ++ msg
 
-echomS :: (MonadDeepError e RpcError m, MonadRibo m, Nvim m, Show a) => a -> m ()
+echomS ::
+  MonadRibo m =>
+  NvimE e m =>
+  Show a =>
+  a ->
+  m ()
 echomS = echom . show
