fc340e5fcb677b87a27281d8ffb9610ceec19db2
Torsten Schmits <torstenschmits@gmail.com>
2019-04-13 15:55:31 +0200

    `echo`

--- modules/core/lib/Ribosome/Api/Echo.hs
+++ modules/core/lib/Ribosome/Api/Echo.hs
@@ -4,14 +4,30 @@ import Ribosome.Control.Monad.Ribo (MonadRibo, NvimE, pluginName)
 import Ribosome.Data.String (escapeQuotes)
 import Ribosome.Nvim.Api.IO (vimCommand)
 
+echoWith :: NvimE e m => String -> String -> m ()
+echoWith cmd msg =
+  vimCommand $ cmd ++ " '" ++ concatMap escapeQuotes msg ++ "'"
+
+echoWithName :: MonadRibo m => NvimE e m => String -> String -> m ()
+echoWithName cmd msg = do
+  name <- pluginName
+  echoWith cmd $ name ++ ": " ++ msg
+
+echo' :: NvimE e m => String -> m ()
+echo' =
+  echoWith "echo"
+
+echo :: MonadRibo m => NvimE e m => String -> m ()
+echo =
+  echoWithName "echo"
+
 echom' :: NvimE e m => String -> m ()
-echom' msg =
-  vimCommand $ "echom '" ++ concatMap escapeQuotes msg ++ "'"
+echom' =
+  echoWith "echom"
 
 echom :: MonadRibo m => NvimE e m => String -> m ()
-echom msg = do
-  name <- pluginName
-  echom' $ name ++ ": " ++ msg
+echom =
+  echoWithName "echom"
 
 echomS ::
   MonadRibo m =>
