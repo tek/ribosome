aa6ba02ccf051890e976f96c2b13fee62c398406
Torsten Schmits <torstenschmits@gmail.com>
2019-04-14 03:05:15 +0200

    `setCursor`

--- modules/core/lib/Ribosome/Api/Window.hs
+++ modules/core/lib/Ribosome/Api/Window.hs
@@ -4,7 +4,7 @@ import Control.Monad (when)
 
 import Ribosome.Control.Monad.Ribo (NvimE)
 import Ribosome.Nvim.Api.Data (Window)
-import Ribosome.Nvim.Api.IO (nvimGetCurrentWin, nvimWinClose, nvimWinGetCursor, windowIsValid)
+import Ribosome.Nvim.Api.IO (nvimGetCurrentWin, nvimWinClose, nvimWinGetCursor, nvimWinSetCursor, windowIsValid)
 
 closeWindow ::
   NvimE e m =>
@@ -33,3 +33,12 @@ currentLine ::
   m Int
 currentLine =
   fst <$> (cursor =<< nvimGetCurrentWin)
+
+setCursor ::
+  NvimE e m =>
+  Window ->
+  Int ->
+  Int ->
+  m ()
+setCursor window line col =
+  nvimWinSetCursor window (line + 1, col)
