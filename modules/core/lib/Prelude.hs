aa3a952f2eb1170c8fcf694beddd2b15e6eb2f06
Torsten Schmits <torstenschmits@gmail.com>
2019-04-14 15:47:38 +0200

    export debug print function in Prelude

--- modules/core/lib/Prelude.hs
+++ modules/core/lib/Prelude.hs
@@ -3,6 +3,8 @@ module Prelude (
   module Control.Monad.DeepState,
   module Data.Foldable,
   module Relude,
+  dbg,
+  dbgs,
   mapLeft,
   undefined,
   (<$$>),
@@ -15,4 +17,14 @@ import Data.Foldable (foldl, traverse_)
 import Data.Functor (void)
 import Data.Functor.Syntax ((<$$>))
 import GHC.Err (undefined)
+import GHC.IO.Unsafe (unsafePerformIO)
 import Relude hiding (undefined, Type, state, modify, gets, get, put)
+
+dbg :: Monad m => Text -> m ()
+dbg msg = do
+  () <- return $ unsafePerformIO (putStrLn msg)
+  return ()
+
+dbgs :: Monad m => Show a => a -> m ()
+dbgs =
+  dbg . show
