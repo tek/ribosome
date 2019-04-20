5407dfa69cfe937fda887f56a3ef8f02b7f30818
Torsten Schmits <torstenschmits@gmail.com>
2019-04-20 02:07:31 +0200

    hide custom Prelude

---
+++ modules/core/lib/Ribosome/Prelude.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Ribosome.Prelude (
+  module Control.Monad.DeepError,
+  module Control.Monad.DeepState,
+  module Data.Foldable,
+  module Relude,
+  dbg,
+  dbgs,
+  mapLeft,
+  undefined,
+  (<$$>),
+) where
+
+import Control.Monad.DeepError (MonadDeepError(throwHoist), hoistEither)
+import Control.Monad.DeepState (MonadDeepState, get, getL, gets, getsL, modify, put, setL)
+import Data.Either.Combinators (mapLeft)
+import Data.Foldable (foldl, traverse_)
+import Data.Functor (void)
+import Data.Functor.Syntax ((<$$>))
+import GHC.Err (undefined)
+import GHC.IO.Unsafe (unsafePerformIO)
+import Relude hiding (undefined, Type, state, modify, gets, get, put)
+
+dbg :: Monad m => Text -> m ()
+dbg msg = do
+  () <- return $ unsafePerformIO (putStrLn msg)
+  return ()
+
+dbgs :: Monad m => Show a => a -> m ()
+dbgs =
+  dbg . show
