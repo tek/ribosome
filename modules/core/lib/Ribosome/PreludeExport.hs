974cfb46d781b774b9aefd04fa5cebfcd8387aa1
Torsten Schmits <torstenschmits@gmail.com>
2019-04-22 00:23:13 +0200

    `PreludeExport` module for consumers

---
+++ modules/core/lib/Ribosome/PreludeExport.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Ribosome.PreludeExport (
+  module Ribosome.Prelude,
+  module Ribosome.Control.Monad.Ribo,
+) where
+
+import Ribosome.Control.Monad.Ribo (MonadRibo, NvimE)
+import Ribosome.Prelude
