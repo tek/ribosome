69245e0eed31af11731a3bdb74e7d09c2e203f26
Torsten Schmits <torstenschmits@gmail.com>
2019-04-22 00:19:15 +0200

    `ReportError PersistError`

--- modules/core/lib/Ribosome/Data/PersistError.hs
+++ modules/core/lib/Ribosome/Data/PersistError.hs
@@ -2,8 +2,12 @@
 
 module Ribosome.Data.PersistError where
 
-import Control.Lens (makeClassyPrisms)
+import System.Log (Priority(INFO, NOTICE, ERROR))
 
+import Ribosome.Data.ErrorReport (ErrorReport(ErrorReport))
+import Ribosome.Error.Report.Class (ReportError(..))
+
+-- TODO use Path
 data PersistError =
   FileNotReadable FilePath
   |
@@ -12,4 +16,18 @@ data PersistError =
   Decode FilePath Text
   deriving (Eq, Show)
 
-makeClassyPrisms ''PersistError
+deepPrisms ''PersistError
+
+instance ReportError PersistError where
+  errorReport (FileNotReadable path) =
+    ErrorReport msg ["PersistError.FileNotReadable:", toText path] NOTICE
+    where
+      msg = "persistence file not readable: " <> toText path
+  errorReport (NoSuchFile path) =
+    ErrorReport msg ["PersistError.SoSuchFile:", toText path] INFO
+    where
+      msg = "no persistence file present at " <> toText path
+  errorReport (Decode path err) =
+    ErrorReport msg ["PersistError.Decode:", toText path, err] ERROR
+    where
+      msg = "invalid data in persistence file, please delete it: " <> toText path
