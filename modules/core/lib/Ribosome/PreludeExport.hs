a3cec1f9499f1aaf35dcf55db14ea86d44784c43
Torsten Schmits <torstenschmits@gmail.com>
2019-04-29 23:52:55 +0200

    export `toMsgpack`, `fromMsgpack`

--- modules/core/lib/Ribosome/PreludeExport.hs
+++ modules/core/lib/Ribosome/PreludeExport.hs
@@ -4,8 +4,12 @@ module Ribosome.PreludeExport (
   module Ribosome.Prelude,
   module Ribosome.Control.Monad.Ribo,
   module Ribosome.Log,
+  toMsgpack,
+  fromMsgpack,
 ) where
 
 import Ribosome.Control.Monad.Ribo (MonadRibo, NvimE)
 import Ribosome.Log (logDebug, logError, logInfo)
+import Ribosome.Msgpack.Decode (fromMsgpack)
+import Ribosome.Msgpack.Encode (toMsgpack)
 import Ribosome.Prelude
