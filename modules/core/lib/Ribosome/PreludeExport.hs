c5b3ed729a37f9f83a6a3beea4a1dea0cbf547ce
Torsten Schmits <torstenschmits@gmail.com>
2019-06-01 12:12:44 +0200

    some exports

--- modules/core/lib/Ribosome/PreludeExport.hs
+++ modules/core/lib/Ribosome/PreludeExport.hs
@@ -4,14 +4,16 @@ module Ribosome.PreludeExport (
   module Ribosome.Prelude,
   module Ribosome.Control.Monad.Ribo,
   module Ribosome.Log,
-  toMsgpack,
-  fromMsgpack,
+  module Ribosome.Nvim.Api.RpcCall,
+  module Ribosome.Msgpack.Decode,
+  module Ribosome.Msgpack.Encode,
   sleep,
 ) where
 
 import Ribosome.Control.Monad.Ribo (MonadRibo, NvimE)
-import Ribosome.Log (logDebug, logError, logInfo)
-import Ribosome.Msgpack.Decode (fromMsgpack)
-import Ribosome.Msgpack.Encode (toMsgpack)
+import Ribosome.Log (logDebug, logError, logInfo, showDebug)
+import Ribosome.Msgpack.Decode (MsgpackDecode(fromMsgpack))
+import Ribosome.Msgpack.Encode (MsgpackEncode(toMsgpack))
+import Ribosome.Nvim.Api.RpcCall (RpcError(..))
 import Ribosome.Prelude
 import Ribosome.System.Time (sleep)
