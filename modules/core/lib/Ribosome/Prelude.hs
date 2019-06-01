06c558b1565be5a833f3e3e9d42a6d0ac0b6ddc2
Torsten Schmits <torstenschmits@gmail.com>
2019-06-01 12:12:25 +0200

    export `hostMaybe`

--- modules/core/lib/Ribosome/Prelude.hs
+++ modules/core/lib/Ribosome/Prelude.hs
@@ -24,7 +24,7 @@ module Ribosome.Prelude (
 ) where
 
 import Control.Lens (makeClassy)
-import Control.Monad.DeepError (MonadDeepError(throwHoist), catchAs, catchAt, hoistEither, ignoreError)
+import Control.Monad.DeepError (MonadDeepError(throwHoist), catchAs, catchAt, hoistEither, hoistMaybe, ignoreError)
 import Control.Monad.DeepState (
   MonadDeepState,
   get,
