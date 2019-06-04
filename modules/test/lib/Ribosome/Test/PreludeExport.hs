71f8d3ee879f3298ec61b36fca649b5668886c4c
Torsten Schmits <torstenschmits@gmail.com>
2019-06-04 19:27:53 +0200

    `PreludeExport` for test

---
+++ modules/test/lib/Ribosome/Test/PreludeExport.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Ribosome.Test.PreludeExport (
+  module Ribosome.PreludeExport,
+  module Ribosome.Test.Unit,
+  module Ribosome.Test.Await,
+) where
+
+import Ribosome.PreludeExport
+import Ribosome.Test.Await
+import Ribosome.Test.Unit
