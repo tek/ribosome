65d3d8f655cd65224ad77aa0eb1d22626f60817a
Torsten Schmits <torstenschmits@gmail.com>
2019-06-04 19:27:34 +0200

    `undo`

---
+++ modules/core/lib/Ribosome/Api/Undo.hs
@@ -0,0 +1,10 @@
+module Ribosome.Api.Undo where
+
+import Ribosome.Control.Monad.Ribo (NvimE)
+import Ribosome.Nvim.Api.IO (vimCommand)
+
+undo ::
+  NvimE e m =>
+  m ()
+undo =
+  vimCommand "undo"
