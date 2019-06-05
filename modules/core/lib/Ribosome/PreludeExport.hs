5cc31aa8814a72e21a1d2e60703269c1fb4e26be
Torsten Schmits <torstenschmits@gmail.com>
2019-06-05 17:54:54 +0200

    don't export ctors of `RpcCall`

--- modules/core/lib/Ribosome/PreludeExport.hs
+++ modules/core/lib/Ribosome/PreludeExport.hs
@@ -14,6 +14,6 @@ import Ribosome.Control.Monad.Ribo (MonadRibo, NvimE)
 import Ribosome.Log (logDebug, logError, logInfo, showDebug)
 import Ribosome.Msgpack.Decode (MsgpackDecode(fromMsgpack))
 import Ribosome.Msgpack.Encode (MsgpackEncode(toMsgpack))
-import Ribosome.Nvim.Api.RpcCall (RpcError(..))
+import Ribosome.Nvim.Api.RpcCall (RpcError)
 import Ribosome.Prelude
 import Ribosome.System.Time (sleep)
