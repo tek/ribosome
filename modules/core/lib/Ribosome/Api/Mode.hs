405cd9a754f62ce91027a11bd9777b4becd5cc40
Torsten Schmits <torstenschmits@gmail.com>
2019-06-06 18:48:43 +0200

    access to the current mode

---
+++ modules/core/lib/Ribosome/Api/Mode.hs
@@ -0,0 +1,38 @@
+module Ribosome.Api.Mode where
+
+import Ribosome.Control.Monad.Ribo (NvimE)
+import Ribosome.Msgpack.Decode (MsgpackDecode(..), msgpackFromString)
+import Ribosome.Nvim.Api.IO (vimCallFunction)
+
+data NvimMode =
+  Normal
+  |
+  Visual
+  |
+  Insert
+  |
+  Other Text
+  deriving (Eq, Show)
+
+instance IsString NvimMode where
+  fromString "n" = Normal
+  fromString "v" = Visual
+  fromString "V" = Visual
+  fromString "CTRL-V" = Visual
+  fromString "i" = Insert
+  fromString a = Other (toText a)
+
+instance MsgpackDecode NvimMode where
+  fromMsgpack = msgpackFromString "NvimMode"
+
+mode ::
+  NvimE e m =>
+  m NvimMode
+mode =
+  vimCallFunction "mode" []
+
+visualModeActive ::
+  NvimE e m =>
+  m Bool
+visualModeActive =
+  (== Visual) <$> mode
