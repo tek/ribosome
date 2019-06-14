24a3efeefc77ee1433c92b64687a8e19cee664f5
Torsten Schmits <torstenschmits@gmail.com>
2019-06-14 22:54:22 +0200

    export `Test.Embed`

--- modules/test/lib/Ribosome/Test/PreludeExport.hs
+++ modules/test/lib/Ribosome/Test/PreludeExport.hs
@@ -3,9 +3,11 @@
 module Ribosome.Test.PreludeExport (
   module Ribosome.PreludeExport,
   module Ribosome.Test.Unit,
+  module Ribosome.Test.Embed,
   module Ribosome.Test.Await,
 ) where
 
 import Ribosome.PreludeExport
 import Ribosome.Test.Await
+import Ribosome.Test.Embed
 import Ribosome.Test.Unit
--- test/u/TestError.hs
+++ test/u/TestError.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DeriveAnyClass #-}
 
 module TestError where
