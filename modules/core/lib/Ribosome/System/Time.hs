993351871afcfb041526a977dcc19a884cb2e438
Torsten Schmits <torstenschmits@gmail.com>
2019-06-14 22:54:00 +0200

    helper for creating `ElapsedP` from `Double`

--- modules/core/lib/Ribosome/System/Time.hs
+++ modules/core/lib/Ribosome/System/Time.hs
@@ -1,12 +1,12 @@
 module Ribosome.System.Time where
 
 import Control.Concurrent (threadDelay)
-import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.Hourglass (Elapsed(Elapsed), ElapsedP(ElapsedP), NanoSeconds(NanoSeconds), Seconds(Seconds))
 import Data.Time.Clock.POSIX (getPOSIXTime)
 import GHC.Float (word2Double)
 
 epochSeconds :: MonadIO m => m Int
-epochSeconds = liftIO $ fmap round getPOSIXTime
+epochSeconds = liftIO $ round <$> getPOSIXTime
 
 usleep :: MonadIO m => Double -> m ()
 usleep =
@@ -18,3 +18,10 @@ sleep seconds =
 
 sleepW :: MonadIO m => Word -> m ()
 sleepW = sleep . word2Double
+
+secondsP :: Double -> ElapsedP
+secondsP s =
+  ElapsedP (Elapsed (Seconds $ floor s)) (NanoSeconds nano)
+  where
+    nano =
+      round (s * 1000000000) `mod` 1000000000
