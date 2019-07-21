8c4bfe597d744508f57f15a407d6aafedfeb686e
Torsten Schmits <torstenschmits@gmail.com>
2019-07-21 12:53:57 +0200

    add tmux to test prelude export

--- modules/test/lib/Ribosome/Test/PreludeExport.hs
+++ modules/test/lib/Ribosome/Test/PreludeExport.hs
@@ -2,12 +2,14 @@
 
 module Ribosome.Test.PreludeExport (
   module Ribosome.PreludeExport,
-  module Ribosome.Test.Unit,
-  module Ribosome.Test.Embed,
   module Ribosome.Test.Await,
+  module Ribosome.Test.Embed,
+  module Ribosome.Test.Tmux,
+  module Ribosome.Test.Unit,
 ) where
 
 import Ribosome.PreludeExport
 import Ribosome.Test.Await
 import Ribosome.Test.Embed
+import Ribosome.Test.Tmux
 import Ribosome.Test.Unit
