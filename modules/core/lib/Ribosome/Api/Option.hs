d46f7e18bc58f4cb92f4646faad0e4f5cfb55a9f
Torsten Schmits <torstenschmits@gmail.com>
2019-08-10 23:57:55 +0200

    `withOption` that brackets with an option set and resets afterwards

--- modules/core/lib/Ribosome/Api/Option.hs
+++ modules/core/lib/Ribosome/Api/Option.hs
@@ -1,8 +1,10 @@
 module Ribosome.Api.Option where
 
+import Control.Exception.Lifted (bracket)
 import Data.Text (splitOn)
+
 import Ribosome.Control.Monad.Ribo (NvimE)
-import Ribosome.Msgpack.Encode (toMsgpack)
+import Ribosome.Msgpack.Encode (MsgpackEncode(toMsgpack))
 import Ribosome.Nvim.Api.IO (vimGetOption, vimSetOption)
 
 optionCat :: NvimE e m => Text -> Text -> m ()
@@ -20,3 +22,19 @@ optionList :: NvimE e m => Text -> m [Text]
 optionList name = do
   s <- vimGetOption name
   return $ splitOn "," s
+
+withOption ::
+  NvimE e m =>
+  MsgpackEncode a =>
+  MonadBaseControl IO m =>
+  Text ->
+  a ->
+  m b ->
+  m b
+withOption name value =
+  bracket set reset . const
+  where
+    set =
+      vimGetOption name <* vimSetOption name (toMsgpack value)
+    reset =
+      vimSetOption name
