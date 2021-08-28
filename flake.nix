53b0e071bc326850f02da45b7db2b9bbb117d1ce
Torsten Schmits <torstenschmits@gmail.com>
2021-08-28 22:18:15 +0200

    bump nixpkgs, nvim

--- flake.nix
+++ flake.nix
@@ -1,19 +1,16 @@
 {
   description = "High Level Neovim Plugin Framework";
 
+  inputs.hix.url = github:tek/hix;
   inputs.chiasma.url = github:tek/chiasma;
 
-  outputs = { chiasma, ... }:
+  outputs = { hix, chiasma, ... }:
   let
-    inherit (chiasma.inputs) hix;
     overrides = { hackage, source, only, configure, pkgs, ... }: {
       chiasma = source.package chiasma "chiasma";
       cornea = hackage "0.4.0.0" "1w9rkf6f861kknkskywb8fczlk7az8m56i3hvmg6a5inpvqf6p7i";
-      nvim-hs = only "8.6.5" (hackage "2.1.0.4" "1fpjrfgmbmkd2qi9mjfjkidgcf8cbqzpaqw36zy2fznm3qkhpss7");
-      unliftio-core = only "8.6.5" (hackage "0.2.0.1" "06cbv2yx5a6qj4p1w91q299r0yxv96ms72xmjvkpm9ic06ikvzzq");
       ribosome = configure "--extra-prog-path=${pkgs.neovim}/bin";
       ribosome-test = drv: drv.overrideAttrs (old: { buildInputs = old.buildInputs ++ [pkgs.neovim pkgs.tmux]; });
-      posix-pty = only "8.6.5" (hackage "0.2.1.1" "0rwb7fj7134lc04dv9vn4j6nq0vadl2qhzlz63pd0an9vqmxw685");
     };
   in hix.flake {
     base = ./.;
@@ -28,5 +25,6 @@
     runConfig = p: {
       extraShellInputs = [p.pkgs.neovim];
     };
+    ghcid.easy-hls = false;
   };
 }
--- packages/ribosome/lib/Ribosome/Nvim/Api/Data.hs
+++ packages/ribosome/lib/Ribosome/Nvim/Api/Data.hs
@@ -1,10 +1,14 @@
 module Ribosome.Nvim.Api.Data where
 
-import Data.MessagePack (Object(ObjectExt))
+import Data.MessagePack (Object (ObjectExt))
 import Prelude
-import Ribosome.Msgpack.Decode (MsgpackDecode(..))
-import Ribosome.Msgpack.Encode (MsgpackEncode(..))
 
+import Ribosome.Msgpack.Decode (MsgpackDecode (..))
+import Ribosome.Msgpack.Encode (MsgpackEncode (..))
 import Ribosome.Nvim.Api.GenerateData (generateData)
+
+data LuaRef =
+  LuaRef Int64
+  deriving (Generic, MsgpackDecode, MsgpackEncode)
 
 generateData
