04daf8ffc164c8f1181329af43975483a310cfc8
Torsten Schmits <git@tryp.io>
2022-06-11 11:58:22 +0200

    export command arg types

--- packages/host/lib/Ribosome/Host.hs
+++ packages/host/lib/Ribosome/Host.hs
@@ -4,11 +4,17 @@ module Ribosome.Host (
   module Ribosome.Host.Class.Msgpack.Decode,
   module Ribosome.Host.Class.Msgpack.Encode,
   module Ribosome.Host.Class.Msgpack.Map,
+  module Ribosome.Host.Data.Args,
+  module Ribosome.Host.Data.Bang,
+  module Ribosome.Host.Data.Bar,
   module Ribosome.Host.Data.BootError,
+  module Ribosome.Host.Data.CommandMods,
+  module Ribosome.Host.Data.CommandRegister,
   module Ribosome.Host.Data.Execution,
   module Ribosome.Host.Data.HandlerError,
   module Ribosome.Host.Data.HostConfig,
   module Ribosome.Host.Data.HostError,
+  module Ribosome.Host.Data.Range,
   module Ribosome.Host.Data.RpcError,
   module Ribosome.Host.Data.RpcHandler,
   module Ribosome.Host.Data.StoredError,
@@ -25,7 +31,12 @@ import Ribosome.Host.Class.Msgpack.Array (msgpackArray)
 import Ribosome.Host.Class.Msgpack.Decode (MsgpackDecode (fromMsgpack))
 import Ribosome.Host.Class.Msgpack.Encode (MsgpackEncode (toMsgpack))
 import Ribosome.Host.Class.Msgpack.Map (msgpackMap)
+import Ribosome.Host.Data.Args (Args (Args))
+import Ribosome.Host.Data.Bang (Bang (Bang, NoBang))
+import Ribosome.Host.Data.Bar (Bar (Bar))
 import Ribosome.Host.Data.BootError (BootError (BootError))
+import Ribosome.Host.Data.CommandMods (CommandMods (CommandMods))
+import Ribosome.Host.Data.CommandRegister (CommandRegister (CommandRegister))
 import Ribosome.Host.Data.Execution (Execution (Async, Sync))
 import Ribosome.Host.Data.HandlerError (
   ErrorMessage (ErrorMessage),
@@ -39,6 +50,7 @@ import Ribosome.Host.Data.HandlerError (
   )
 import Ribosome.Host.Data.HostConfig (HostConfig (..), LogConfig (..), setStderr)
 import Ribosome.Host.Data.HostError (HostError (..))
+import Ribosome.Host.Data.Range (Range (Range), RangeStyle (..))
 import Ribosome.Host.Data.RpcError (RpcError (RpcError))
 import Ribosome.Host.Data.RpcHandler (Handler, RpcHandler (RpcHandler), simpleHandler)
 import Ribosome.Host.Data.StoredError (StoredError (StoredError))
