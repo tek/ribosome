7d474af63e913f2832c76adbc68586b6fd3a4f5b
Torsten Schmits <git@tryp.io>
2022-06-11 21:26:56 +0200

    export PersistPath in Persist

--- packages/ribosome/lib/Ribosome/Persist.hs
+++ packages/ribosome/lib/Ribosome/Persist.hs
@@ -1,7 +1,9 @@
 module Ribosome.Persist (
   module Ribosome.Effect.Persist,
+  module Ribosome.Effect.PersistPath,
   module Ribosome.Data.PersistError,
 ) where
 
 import Ribosome.Data.PersistError
 import Ribosome.Effect.Persist
+import Ribosome.Effect.PersistPath
