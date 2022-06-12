d0cba2b97f63671e235af922387a41d5c0742362
Torsten Schmits <git@tryp.io>
2022-06-12 15:02:50 +0200

    add functions for parsing paths relative to nvim's cwd

--- packages/ribosome/lib/Ribosome/Api/Path.hs
+++ packages/ribosome/lib/Ribosome/Api/Path.hs
@@ -1,10 +1,10 @@
 module Ribosome.Api.Path where
 
-import Path (Abs, Dir, Path)
-
-import Ribosome.Host.Api.Effect (vimCallFunction, nvimCommand)
-import Ribosome.Host.Effect.Rpc (Rpc)
 import Exon (exon)
+import Path (Abs, Dir, File, Path, SomeBase (Abs, Rel), parseSomeDir, parseSomeFile, (</>))
+
+import Ribosome.Host.Api.Effect (nvimCommand, vimCallFunction)
+import Ribosome.Host.Effect.Rpc (Rpc)
 import Ribosome.Path (pathText)
 
 nvimCwd ::
@@ -19,3 +19,28 @@ nvimSetCwd ::
   Sem r ()
 nvimSetCwd dir =
   nvimCommand [exon|cd #{pathText dir}|]
+
+nvimRelativePath ::
+  Member Rpc r =>
+  SomeBase t ->
+  Sem r (Path Abs t)
+nvimRelativePath = \case
+  Abs p ->
+    pure p
+  Rel p -> do
+    cwd <- nvimCwd
+    pure (cwd </> p)
+
+parseNvimDir ::
+  Member Rpc r =>
+  Text ->
+  Sem r (Maybe (Path Abs Dir))
+parseNvimDir =
+  traverse nvimRelativePath . parseSomeDir . toString
+
+parseNvimFile ::
+  Member Rpc r =>
+  Text ->
+  Sem r (Maybe (Path Abs File))
+parseNvimFile =
+  traverse nvimRelativePath . parseSomeFile . toString
