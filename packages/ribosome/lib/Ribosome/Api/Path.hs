e83ca43ebf17fec5318b6a0222c56587207c7e9e
Torsten Schmits <git@tryp.io>
2022-06-12 03:53:34 +0200

    add nvimSetCwd

--- packages/ribosome/lib/Ribosome/Api/Path.hs
+++ packages/ribosome/lib/Ribosome/Api/Path.hs
@@ -2,11 +2,20 @@ module Ribosome.Api.Path where
 
 import Path (Abs, Dir, Path)
 
-import Ribosome.Host.Api.Effect (vimCallFunction)
+import Ribosome.Host.Api.Effect (vimCallFunction, nvimCommand)
 import Ribosome.Host.Effect.Rpc (Rpc)
+import Exon (exon)
+import Ribosome.Path (pathText)
 
 nvimCwd ::
   Member Rpc r =>
   Sem r (Path Abs Dir)
 nvimCwd =
   vimCallFunction "getcwd" []
+
+nvimSetCwd ::
+  Member Rpc r =>
+  Path Abs Dir ->
+  Sem r ()
+nvimSetCwd dir =
+  nvimCommand [exon|cd #{pathText dir}|]
