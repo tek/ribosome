3e3083c703653cc00121ed970fc7086a59d0affb
Torsten Schmits <git@tryp.io>
2022-07-27 22:31:21 +0200

    docs

--- packages/test/lib/Ribosome/Test/Error.hs
+++ packages/test/lib/Ribosome/Test/Error.hs
@@ -1,3 +1,5 @@
+{-# options_haddock not-home #-}
+
 module Ribosome.Test.Error where
 
 import Polysemy.Test (TestError (TestError))
@@ -5,14 +7,16 @@ import Polysemy.Test (TestError (TestError))
 import Ribosome.Host.Data.HandlerError (ToErrorMessage, handlerErrorMessage, mapHandlerError)
 import Ribosome.Host.Data.RpcHandler (Handler)
 
+-- |Resume an effect and convert its error from @'Stop' err@ to @'Error' 'TestError'@.
 resumeTestError ::
-  ∀ eff e r .
-  Show e =>
-  Members [eff !! e, Error TestError] r =>
+  ∀ eff err r .
+  Show err =>
+  Members [eff !! err, Error TestError] r =>
   InterpreterFor eff r
 resumeTestError =
   resumeHoistError (TestError . show)
 
+-- |Run a 'Handler', converting the @'Stop' 'HandlerError'@ at its head to @'Error' 'TestError'@.
 testHandler ::
   Member (Error TestError) r =>
   Handler r a ->
@@ -20,6 +24,8 @@ testHandler ::
 testHandler =
   stopToErrorWith (TestError . handlerErrorMessage)
 
+-- |Run a 'Handler' in a new thread and return an action that waits for the thread to terminate when sequenced.
+-- Converts the @'Stop' 'HandlerError'@ at its head to @'Error' 'TestError'@ when it is awaited.
 testHandlerAsync ::
   Members [Error TestError, Async] r =>
   Handler r a ->
@@ -30,11 +36,12 @@ testHandlerAsync h = do
   pure do
     testHandler . stopEither =<< note (TestError "async handler didn't produce result") =<< await thread
 
+-- |Interpret @'Stop' err@ to @'Error' 'TestError'@ by using @err@'s instance of 'ToErrorMessage'.
 testError ::
-  ∀ e r a .
-  ToErrorMessage e =>
+  ∀ err r a .
+  ToErrorMessage err =>
   Member (Error TestError) r =>
-  Sem (Stop e : r) a ->
+  Sem (Stop err : r) a ->
   Sem r a
 testError =
   testHandler . mapHandlerError . raiseUnder
