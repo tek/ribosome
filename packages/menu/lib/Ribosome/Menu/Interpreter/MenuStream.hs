cf5982d8d57057e26c7c87842b974e9798e66d2d
Torsten Schmits <git@tryp.io>
2022-09-21 02:18:29 +0200

    tweak prompt accumulator

--- packages/menu/lib/Ribosome/Menu/Interpreter/MenuStream.hs
+++ packages/menu/lib/Ribosome/Menu/Interpreter/MenuStream.hs
@@ -4,17 +4,11 @@ import qualified Data.List.NonEmpty as NonEmpty
 import Polysemy.Final (bindS, getInitialStateS, getInspectorS, interpretFinal, runS)
 import qualified Streamly.Data.Fold as Fold
 import qualified Streamly.Internal.Data.Stream.IsStream as Stream
-import Streamly.Internal.Data.Stream.IsStream (after_)
 
 import Ribosome.Menu.Effect.MenuStream (MenuStream (MenuStream))
 import Ribosome.Menu.Stream.Accumulate (mapMAcc)
 import Ribosome.Menu.Stream.Util (repeatUntilNothing)
 
--- TODO mapMAcc should probably be called on a combined stream of prompt and items, with items being on the Right side,
--- short-circuiting the accumulation.
--- the way it is, a query update will be executed while items are being processed, which will cause it to wait for the
--- lock, while further prompt updates will be queued and discharged right after the first one, causing one unnecessary
--- additional filtering.
 interpretMenuStream ::
   Member (Final IO) r =>
   InterpreterFor MenuStream r
@@ -36,22 +30,23 @@ interpretMenuStream =
         maybeF =
           fmap (<$ s) <=< ins
         prompt =
-          mapMAcc (pure . Left) (queryUpdate . NonEmpty.last) $
           repeatUntilNothing (maybeF <$> promptEvents)
+        insert new =
+          insertItems (pureS new)
+        chunker = pure . \case
+          [] ->
+            Fold.take 100 Fold.toList
+          _ ->
+            Fold.take 10000 Fold.toList
         menuItems =
-          after_ exhausted $
-          Stream.mapM insert $
           Stream.foldIterateM chunker (pure []) $
           Stream.fromSerial items
-          where
-            insert new =
-              insertItems (pureS new)
-            chunker = pure . \case
-              [] ->
-                Fold.take 100 Fold.toList
-              _ ->
-                Fold.take 10000 Fold.toList
+        triage = \case
+          Left p -> pure (Left p)
+          Right (Just i) -> Right . Just <$> insert i
+          Right Nothing -> Right Nothing <$ exhausted
       pure $
         fmap pureS $
-        Stream.fold (Fold.drainBy renderEvent) $
-        Stream.parallelFst prompt menuItems
+        Stream.fold (Fold.drainBy (traverse_ renderEvent)) $
+        mapMAcc triage (fmap Just . queryUpdate . NonEmpty.last) $
+        Stream.parallelFst (Left <$> prompt) (Right <$> Stream.serial (Just <$> menuItems) (Stream.fromPure Nothing))
