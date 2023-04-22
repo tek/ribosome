85b7fe3dde25264c057d4cde5670f8b2e385dc49
Torsten Schmits <git@tryp.io>
2023-04-22 16:35:30 +0200

    fix concurrency in prompt accumulator

--- packages/menu/lib/Ribosome/Menu/Stream/Accumulate.hs
+++ packages/menu/lib/Ribosome/Menu/Stream/Accumulate.hs
@@ -5,6 +5,10 @@ import Prelude hiding (consume, finally, output)
 import qualified Streamly.Internal.Data.Stream.IsStream as Stream
 import Streamly.Prelude (IsStream, SerialT)
 
+overMVar :: MVar a -> (a -> (a, b)) -> IO b
+overMVar v f =
+  modifyMVar v (pure . f)
+
 data BusyState =
   Idle
   |
@@ -16,20 +20,6 @@ data BusyEnv r a =
     buffer :: Seq a,
     state :: BusyState
   }
-
-nonEmptyBuffer :: Seq a -> a -> NonEmpty a
-nonEmptyBuffer (h :<| t) a =
-  h :| (toList (t |> a))
-nonEmptyBuffer Empty a =
-  pure a
-
-emitBuffer ::
-  (NonEmpty a -> IO r) ->
-  Seq a ->
-  a ->
-  SerialT IO r
-emitBuffer handleAcc buffer a =
-  Stream.fromEffect (handleAcc (nonEmptyBuffer buffer a))
 
 increment :: BusyEnv r a -> BusyEnv r a
 increment env =
@@ -48,6 +38,20 @@ decrement BusyEnv {..} =
       Busy 1 -> Idle
       Busy n -> Busy (n - 1)
 
+nonEmptyBuffer :: Seq a -> a -> NonEmpty a
+nonEmptyBuffer (h :<| t) a =
+  h :| (toList (t |> a))
+nonEmptyBuffer Empty a =
+  pure a
+
+emitBuffer ::
+  (NonEmpty a -> IO r) ->
+  Seq a ->
+  a ->
+  SerialT IO r
+emitBuffer handleAcc buffer a =
+  Stream.fromEffect (handleAcc (nonEmptyBuffer buffer a))
+
 bufferedElement ::
   (NonEmpty a -> IO r) ->
   a ->
@@ -59,30 +63,30 @@ bufferedElement handleAcc a = \case
   BusyEnv {buffer, state = Busy n} ->
     (BusyEnv {buffer = buffer |> a, state = Busy n}, Stream.nil)
 
-elemStep ::
+element ::
   (NonEmpty a -> IO r) ->
   (b -> IO r) ->
   MVar (BusyEnv r a) ->
   Either a b ->
-  SerialT IO r
-elemStep handleAcc handleReg envVar el =
-  Stream.concatM $ modifyMVar envVar \ env ->
+  SerialT IO (SerialT IO r)
+element handleAcc handleReg envVar el =
+  Stream.fromEffect $ overMVar envVar \ env ->
     case el of
       Left a ->
-        pure (bufferedElement handleAcc a env)
+        (bufferedElement handleAcc a env)
       Right b ->
-        pure (increment env, Stream.fromEffect (handleReg b))
+        (increment env, Stream.fromEffect (handleReg b))
 
 checkBuffer ::
   (NonEmpty a -> IO r) ->
   MVar (BusyEnv r a) ->
   SerialT IO r
 checkBuffer handleAcc envVar =
-  Stream.concatM $ modifyMVar envVar $ pure . \case
+  Stream.concatM $ overMVar envVar \case
     env | (buf :|> a) <- env.buffer ->
-      (env {buffer = []}, Stream.serial (emitBuffer handleAcc buf a) (checkBuffer handleAcc envVar))
+      (env {buffer = []}, emitBuffer handleAcc buf a <> checkBuffer handleAcc envVar)
         | otherwise ->
-          (decrement env, Stream.nil)
+      (decrement env, Stream.nil)
 
 accLeftBusy ::
   IsStream t =>
@@ -93,5 +97,5 @@ accLeftBusy ::
 accLeftBusy handleAcc handleReg str =
   Stream.bracket_ (liftIO (newMVar (BusyEnv [] Idle))) (const unit) \ envVar ->
     Stream.concatMap (\ r -> Stream.serial r (checkBuffer handleAcc envVar)) $
-    Stream.map (elemStep handleAcc handleReg envVar) $
+    Stream.concatMapWith Stream.ahead (element handleAcc handleReg envVar) $
     Stream.adapt str
